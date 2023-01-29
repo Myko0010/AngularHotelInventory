f200b55c58e2e60c22e1478aca96a86f919669aa
Myko0010 <mikiyastekalegn@gmail.com>
2023-01-29 19:11:57 +0300

    Initial commit

---
+++ src/app/user-tree/user-tree.component.spec.ts
@@ -0,0 +1,32 @@
+import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTreeModule } from '@angular/material/tree';
+
+import { UserTreeComponent } from './user-tree.component';
+
+describe('UserTreeComponent', () => {
+  let component: UserTreeComponent;
+  let fixture: ComponentFixture<UserTreeComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ UserTreeComponent ],
+      imports: [
+        MatButtonModule,
+        MatIconModule,
+        MatTreeModule,
+      ]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(UserTreeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should compile', () => {
+    expect(component).toBeTruthy();
+  });
+});
